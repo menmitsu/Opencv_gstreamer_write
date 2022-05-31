89444559d82405fbd7a9bba3cd069a3c4b8a4ad8
menmitsu <rahul@menmitsu.com>
2022-05-31 20:37:05 +0530

    Initial commit

--- main.cpp
+++ main.cpp
@@ -34,8 +34,11 @@ using namespace std;
 
 int main(int argc, const char ** argv) {
 
+  string gst_in="udpsrc port=5001 ! application/x-rtp, media=(string)video, payload=(int)96, clock-rate=(int)90000, encoding-name=(string)H264, ssrc=(uint)3572028551, timestamp-offset=(uint)600628017, seqnum-offset=(uint)516, a-framerate=(string)30 ! rtph264depay ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=1000000000 ! queue max-size-time=1000000 min-threshold-time=1000000 ! decodebin ! queue !  videoconvert ! appsink";
+  string gst_out="appsrc ! video/x-raw, format=BGR, pixel-aspect-ratio=1/1 ! queue ! videoconvert ! video/x-raw, format=BGRx ! videoconvert ! videoconvert ! video/x-raw,format=I420 ! x264enc pass=qual quantizer=20 tune=zerolatency ! video/x-h264, stream-format=byte-stream ! rtph264pay pt=96 config-interval=1 ! application/x-rtp, media=video, encoding-name=H264 ! udpsink host=34.87.142.156 port=8004 auto-multicast=true";
 
-  VideoCapture video(0);
+
+  VideoCapture video(gst_in);
   Mat colorFrame;
   video>>colorFrame;
 
@@ -43,8 +46,6 @@ int main(int argc, const char ** argv) {
     Size captureSize = Size( (int) video.get(CAP_PROP_FRAME_WIDTH),(int) video.get(CAP_PROP_FRAME_HEIGHT));    //Acquire input size
     VideoWriter outputWriter;
 
-
-  string gst_out="appsrc ! video/x-raw, format=BGR, pixel-aspect-ratio=1/1 ! queue ! videoconvert ! video/x-raw, format=BGRx ! videoconvert ! videoconvert ! video/x-raw,format=I420 ! x264enc pass=qual quantizer=20 tune=zerolatency ! video/x-h264, stream-format=byte-stream ! rtph264pay pt=96 config-interval=1 ! application/x-rtp, media=video, encoding-name=H264 ! udpsink host=34.87.142.156 port=8004 auto-multicast=true";
 
   outputWriter.open(gst_out.c_str(), codec, 30, captureSize, true);
 
